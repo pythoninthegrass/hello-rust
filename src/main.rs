e1510d5d9d217c7774feb550a8137b9b28bf3ea4
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-03-11 16:40:55 -0500

    fix: store env vars as vectors
    Now can iterate over tuples inside vectors avoiding duplicate output

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
 use dotenvy::dotenv;
 use std::env;
 
+// TODO: only return specific env vars
+
 fn main() {
-    // Load .env file
+    // Store environment variables in a vector
+    let env_vars: Vec<_> = env::vars().collect();
+
+    // dotenvy iterator returns result, unwrap to get the value, map
+    // extracts the value from the result, and collect key/value
+    // pairs into a vector
+    let dotenv_vars: Vec<_> = dotenvy::dotenv_iter().unwrap().map(|x| x.unwrap()).collect();
+
+    // Print env vars to stdout
     match dotenv() {
         Ok(_) => {
             // If .env file loaded successfully, greet the user
-            if let Some(name) = env::var("NAME").ok() {
-                println!("Hello, {}!", name);
+            if let Some((_, value)) = env_vars.iter().find(|(key, _)| key == "NAME") {
+                println!("Hello, {}!\n", value);
             } else {
-                println!("Hello, world!");
+                println!("Hello, world!\n");
             }
-            // read remaining env vars other than NAME
-            for item in dotenvy::dotenv_iter().unwrap() {
-               let (key, value) = item.unwrap();
+            // read env vars from env::vars()
+            println!("env vars:");
+            for (key, value) in &env_vars {
                 if key != "NAME" {
-                     println!("{}: {}", key, value);
+                    println!("{}: {}", key, value);
+                }
+            }
+            println!();
+            // read env vars from dotenvy::dotenv_iter()
+            println!("dotenv vars:");
+            for (key, value) in &dotenv_vars {
+                if key != "NAME" {
+                    println!("{}: {}", key, value);
                 }
             }
         },
