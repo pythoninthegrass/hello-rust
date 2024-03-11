93f7f6efbf19ce768f60e649711b27416d7f42f2
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-03-11 15:11:29 -0500

    feat: read from env vars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Load .env file
+    match dotenv() {
+        Ok(_) => {
+            // If .env file loaded successfully, greet the user
+            if let Some(name) = env::var("NAME").ok() {
+                println!("Hello, {}!", name);
+            } else {
+                println!("Hello, world!");
+            }
+        },
+        Err(_) => {
+            // If .env file could not be loaded or vars don't match, print "Hello, world!"
+            println!("Hello, world!");
+        }
+    }
 }
